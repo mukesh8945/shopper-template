d939c6f278e7fc20e569ff714d6cbf9b50cc3b04
Mukesh Kumar <mukeshkumarit2002@gmail.com>
2025-08-20 14:57:41 +0530

    Initialize project using Create React App

---
+++ src/components/ShoppingComponent.js
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react"
+
+export default function ShoppingComponent() {
+    const [categories, setCategories] = useState([]);
+    const [products, setProducts] = useState([]);
+
+    function LoadCategories() {
+        fetch('https://fakestoreapi.com/products/categories')
+            .then(response => response.json())
+            .then(data => {
+                data.unshift('All');
+                setCategories(data);
+            });
+    }
+
+    function LoadProducts() {
+        fetch('https://fakestoreapi.com/products/')
+            .then(response => response.json())
+            .then(data => {
+                setProducts(data);
+            });
+    }
+    // component initiliser
+    useEffect(() => {
+        LoadCategories();
+        LoadProducts();
+    }, [])
+
+
+
+    return (
+        <div className="container-fluid">
+            <header className="bg-danger text-white text-center p-2">
+                <h1><span className="bi bi-cart"></span>Shopping Home</h1>
+            </header>
+            <section className="row">
+                <nav className="col-3">
+                    <div>
+                        <label>Select a Category</label>
+                        <div>
+                            <select className="form-select">
+                                {
+                                    categories.map(category =>
+                                        <option key={category}>{category.toUpperCase()}</option>
+                                    )
+                                }
+
+                            </select>
+                        </div>
+                    </div>
+
+                </nav>
+                <main className="col-9 d-flex flex-wrap">
+                    {
+                        products.map(product =>
+                            <div className="card m-2 p-2 w-25">
+                                <img src={product.image} className="card-img-top" height="150" />
+                                <div className="card-header">
+                                    <p>{product.title}</p>
+                                </div>
+                                <div className="card-body">
+                                    <dl>
+                                        <dt>Price</dt>
+                                        <dd>{product.price}</dd>
+                                        <dt>Rating</dt>
+                                        <dd>
+                                            <span className="bi bi-star-fill text-success"></span>{product.rating.rate}<span>[{product.rating.count}]</span>
+
+                                        </dd>
+                                    </dl>
+                                </div>
+                            </div>
+
+                        )
+                    }
+                </main>
+            </section>
+
+        </div>
+    )
+}
