94cf7eba66169026247bf1289840627c2a6426f8
Mukesh Kumar <mukeshkumarit2002@gmail.com>
2025-08-25 23:36:32 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import './index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import reportWebVitals from './reportWebVitals';
-// import DataBinding from './components/DataBinding';
 import ShoppingComponent from './components/ShoppingComponent';
-// import DataBindingcomponent from './components/DataBindingcomponent';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
