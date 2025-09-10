57a3d44abc0e5df4dcb05af04aacf596cb870131
Mukesh Kumar <mukeshkumarit2002@gmail.com>
2025-09-10 13:51:58 +0530

    yup lib

--- src/components/YupValidation.js
+++ src/components/YupValidation.js
@@ -1,6 +1,8 @@
 import { useFormik } from "formik";
 import * as yup from "yup";
 export default function YupValidation() {
+    const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+
     const formik = useFormik({
         initialValues: {
             UserName: '',
@@ -9,11 +11,14 @@ export default function YupValidation() {
         },
         validationSchema: yup.object({
             UserName: yup.string().required('userName Required').min(4, 'Name too short').max(10, 'Name too Long....'),
+            Email: yup.string().email(),
+            Age: yup.number().required('Number Required').positive().integer(),
+            Mobile: yup.string().matches(phoneRegExp, 'Phone number is not valid')
         }),
         onSubmit: values => {
             alert(JSON.stringify(values));
         }
-    })
+    });
 
     return (
         <div className="container-fluid">
@@ -24,14 +29,14 @@ export default function YupValidation() {
                     <dd><input {...formik.getFieldProps("UserName")} type="text" /></dd>
                     <dd className="text-danger">{formik.errors.UserName}</dd>
                     <dt>Email</dt>
-                    <dd><input type="text" /></dd>
-                    <dd className="text-danger">Email Required</dd>
+                    <dd><input {...formik.getFieldProps("Email")} type="text" /></dd>
+                    <dd className="text-danger">{formik.errors.Email}</dd>
                     <dt>Age</dt>
-                    <dd><input type="text" /></dd>
-                    <dd className="text-danger">Age are Required</dd>
+                    <dd><input{...formik.getFieldProps("Age")} type="text" /></dd>
+                    <dd className="text-danger">{formik.errors.Age}</dd>
                     <dt>Mobile</dt>
-                    <dd><input type="text" /></dd>
-                    <dd className="text-danger">Mobile Number are Required</dd>
+                    <dd><input {...formik.getFieldProps("Mobile")} type="text" /></dd>
+                    <dd className="text-danger">{formik.errors.Mobile}</dd>
                 </dl>
                 <button className="btn btn-success" type="submit">Register</button>
             </form>
