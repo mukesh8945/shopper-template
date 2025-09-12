652c41bd8d596ebc7aced03a25a2518332d440c8
Mukesh Kumar <mukeshkumarit2002@gmail.com>
2025-09-12 13:29:00 +0530

    new page

--- src/components/YupvalidationComponent.js
+++ src/components/YupvalidationComponent.js
@@ -17,7 +17,7 @@ export default function YupvalidationComponent() {
                         UserName: yup.string().min(4, 'Name to short').max(10, 'Name to Long').required('Name Required'),
                         Email: yup.string().email('invalid email').required('Email required'),
                         Age: yup.number().required('Age Required').typeError('Age must be number'),
-                        City: yup.string()
+                        City: yup.string().required('City Name Reqwired')
                     })
                 }
                 onSubmit={
@@ -26,29 +26,37 @@ export default function YupvalidationComponent() {
                     }
                 }
             >
+                {
+                    fields =>
+                        <Form>
+                            {
+                                <div>
+                                    <dl>
+                                        <dt>User Name</dt>
+                                        <dd><Field name="UserName" type="text" /></dd>
+                                        <dd className="text-danger"><ErrorMessage name="UserName" /></dd>
+                                        <dt>Email</dt>
+                                        <dd><Field name="Email" type="text"></Field></dd>
+                                        <dd className="text-danger"><ErrorMessage name="Email" /></dd>
+                                        <dt>Age</dt>
+                                        <dd><Field name="Age" type="text"></Field></dd>
+                                        <dd className="text-danger"><ErrorMessage name="Age" /></dd>
+                                        <dt>City</dt>
+                                        <dd>
+                                            <Field as="select" name="City">
+                                                <option value="">Select a city</option>
+                                                <option value="Delhi">Delhi</option>
+                                                <option value="Hyderabad">Hyderabad</option>
+                                            </Field>
+                                            <ErrorMessage name="City" component="div" className="text-danger"></ErrorMessage>
+                                        </dd>
+                                    </dl>
+                                    <button disabled={(fields.isValid) ? false : true} className="btn btn-success" type="submit">Register</button>
+                                </div>
+                            }
+                        </Form>
+                }
 
-                <Form>
-                    {
-                        <div>
-                            <dl>
-                                <dt>User Name</dt>
-                                <dd><Field name="UserName" type="text" /></dd>
-                                <dt>Email</dt>
-                                <dd><Field name="Email" type="text"></Field></dd>
-                                <dt>Age</dt>
-                                <dd><Field name="Age" type="text"></Field></dd>
-                                <dt>City</dt>
-                                <dd>
-                                    <Field as="select" name="city">
-                                        <option>Delhi</option>
-                                        <option>Hyderabad</option>
-                                    </Field>
-                                </dd>
-                            </dl>
-                            <button>Register</button>
-                        </div>
-                    }
-                </Form>
             </Formik>
 
 
